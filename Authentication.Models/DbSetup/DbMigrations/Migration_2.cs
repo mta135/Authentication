229e1d3b8d2c23b908e29a1dcf5c736f24788b6d
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2024-12-01 01:24:17 +0200

    Adding table to User

---
+++ Authentication.Models/DbSetup/DbMigrations/Migration_2.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Models.DbSetup.DbMigrations
+{
+    [Migration(2)]
+    public class Migration_2 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Column("UserName").FromTable("User");
+        }
+
+        public override void Up()
+        {
+            Alter.Table("User").AddColumn("UserName").AsString(250).Nullable();
+        }
+    }
+}
--- Authentication.Models/DbSetup/DbMigrations/Migrations.cs
+++ Authentication.Models/DbSetup/DbMigrations/Migrations.cs
@@ -1,10 +1,13 @@
-﻿namespace Auth.Models.DbSetup.DataBaseMigrations
+﻿using Authentication.Models.DbSetup.DbMigrations;
+
+namespace Auth.Models.DbSetup.DataBaseMigrations
 {
     public class Migrations
     {
         public Migrations()
         {
             _ = new Migration_1();
+            _ = new Migration_2();
         }
     }
 }
