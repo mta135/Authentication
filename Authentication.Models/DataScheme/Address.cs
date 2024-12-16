aced4d7737062c8907019283a26a164037790c3b
mihai.tamazlicaru <mihai.tamazlicaru@dsi.md>
2024-12-16 21:43:04 +0200

    fix

--- Authentication.Models/DataScheme/Address.cs
+++ Authentication.Models/DataScheme/Address.cs
@@ -7,11 +7,11 @@ public partial class Address
 {
     public int Id { get; set; }
 
-    public string? Street { get; set; }
+    public string Street { get; set; }
 
-    public string? City { get; set; }
+    public string City { get; set; }
 
-    public string? State { get; set; }
+    public string State { get; set; }
 
-    public string? Zip { get; set; }
+    public string Zip { get; set; }
 }
