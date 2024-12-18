c5ff594e7e3e796d26c146530b05dd7fbf26bdbe
mihai.tamazlicaru <mihai.tamazlicaru@dsi.md>
2024-12-18 13:55:23 +0200

    Fix connetion string

--- Authentication.Models/DataScheme/FlowersStoreDbContext.cs
+++ Authentication.Models/DataScheme/FlowersStoreDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Authentication.Models.DbConnection;
 using Microsoft.EntityFrameworkCore;
 
 namespace Authentication.Api;
@@ -26,8 +27,7 @@ public partial class FlowersStoreDbContext : DbContext
     public virtual DbSet<VersionInfo> VersionInfos { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=devdsi;Initial Catalog=FlowersStore;User ID=sa;Password=sa;Trust Server Certificate=True");
+      => optionsBuilder.UseSqlServer(FlowerStoreConnectionSettings.Connection);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
